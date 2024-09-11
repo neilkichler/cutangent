2beb7407cd3868f7721e693d9728d25b2720a072
Neil Kichler <neil.kichler@gmail.com>
2024-09-11 15:22:04 +0200

    initial static array vector mode

---
+++ include/cutangent/arithmetic/basic_v.cuh
@@ -0,0 +1,554 @@
+#ifndef CUTANGENT_ARITHMETIC_BASIC_V_CUH
+#define CUTANGENT_ARITHMETIC_BASIC_V_CUH
+
+#include <cutangent/vtangent.h>
+
+#include <algorithm>
+#include <cmath>
+#include <concepts>
+#include <numbers>
+
+namespace cu
+{
+
+#define fn inline constexpr __device__
+
+template<typename T, int N>
+fn vtangent<T, N> operator-(vtangent<T, N> x)
+{
+    vtangent<T, N> res;
+    res.v = -x.v;
+    for (int i = 0; i < N; ++i) {
+        res.ds[i] = -x.ds[i];
+    }
+    return res;
+}
+
+template<typename T, int N>
+fn vtangent<T, N> operator+(vtangent<T, N> a, vtangent<T, N> b)
+{
+    vtangent<T, N> res;
+    res.v = a.v + b.v;
+    for (int i = 0; i < N; ++i) {
+        res.ds[i] = a.ds[i] + b.ds[i];
+    }
+    return res;
+}
+
+template<typename T, int N>
+fn vtangent<T, N> operator+(vtangent<T, N> a, T b)
+{
+    vtangent<T, N> res;
+    res.v = a.v + b;
+    for (int i = 0; i < N; ++i) {
+        res.ds[i] = a.ds[i];
+    }
+    return res;
+}
+
+// template<typename T>
+// fn tangent<T> operator+(T a, tangent<T> b)
+// {
+//     return { a + b.v, b.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator+(tangent<T> a, auto b)
+// {
+//     return { a.v + b, a.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator+(auto a, tangent<T> b)
+// {
+//     return { a + b.v, b.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator-(tangent<T> a, tangent<T> b)
+// {
+//     return { a.v - b.v, a.d - b.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator-(tangent<T> a, T b)
+// {
+//     return { a.v - b, a.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator-(tangent<T> a, auto b)
+// {
+//     return { a.v - b, a.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator-(T a, tangent<T> b)
+// {
+//     return { a - b.v, -b.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator-(auto a, tangent<T> b)
+// {
+//     return { a - b.v, -b.d };
+// }
+//
+
+template<typename T, int N>
+fn vtangent<T, N> operator*(vtangent<T, N> a, vtangent<T, N> b)
+{
+    vtangent<T, N> res;
+    res.v = a.v * b.v;
+    // printf("a.v = %g\n", a.v);
+    // printf("b.v = %g\n", b.v);
+    for (int i = 0; i < N; ++i) {
+        // printf("a.ds[%d] = %g\n", i, a.ds[i]);
+        // printf("b.ds[%d] = %g\n", i, b.ds[i]);
+        res.ds[i] = a.v * b.ds[i] + a.ds[i] * b.v;
+        // printf("res.ds[%d] = %g\n", i, res.ds[i]);
+    }
+    return res;
+}
+
+// template<typename T>
+// fn tangent<T> operator*(tangent<T> a, tangent<T> b)
+// {
+//     return { a.v * b.v, a.v * b.d + a.d * b.v };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator*(tangent<T> a, T b)
+// {
+//     return { a.v * b, a.d * b };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator*(T a, tangent<T> b)
+// {
+//     return { a * b.v, a * b.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator*(tangent<T> a, std::integral auto b)
+// {
+//     return { a.v * b, a.d * b };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator*(std::integral auto a, tangent<T> b)
+// {
+//     return b * a;
+// }
+//
+// template<typename T>
+// fn tangent<T> operator*(tangent<T> a, auto b)
+// {
+//     return { a.v * b, a.d * b };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator*(auto a, tangent<T> b)
+// {
+//     return b * a;
+// }
+//
+// template<typename T>
+// fn tangent<T> operator/(tangent<T> a, tangent<T> b)
+// {
+//     return { a.v / b.v, (a.d * b.v - a.v * b.d) / (b.v * b.v) };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator/(T a, tangent<T> b)
+// {
+//     return { a / b.v, (-a * b.d) / (b.v * b.v) };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator/(tangent<T> a, T b)
+// {
+//     return { a.v / b, a.d / b };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator/(std::integral auto a, tangent<T> b)
+// {
+//     return { a / b.v, (-a * b.d) / (b.v * b.v) };
+// }
+//
+// template<typename T>
+// fn tangent<T> operator/(tangent<T> a, std::integral auto b)
+// {
+//     return { a.v / b, a.d / b };
+// }
+//
+// template<typename T>
+// fn tangent<T> max(tangent<T> a, tangent<T> b)
+// {
+//     using std::max;
+//
+//     return { max(a.v, b.v),
+//              a.v >= b.v ? a.d : b.d }; // '>=' instead of '>' due to subgradient
+// }
+//
+// template<typename T>
+// fn tangent<T> min(tangent<T> a, tangent<T> b)
+// {
+//     using std::min;
+//
+//     return { min(a.v, b.v),
+//              a.v <= b.v ? a.d : b.d }; // '<=' instead of '<' due to subgradient
+// }
+//
+// template<typename T>
+// fn tangent<T> abs(tangent<T> x)
+// {
+//     using std::abs;
+//     using std::copysign;
+//     // NOTE: not differentiable at x = 0.
+//     return { abs(x.v), copysign(1.0, x.v) * x.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> clamp(tangent<T> v, tangent<T> lb, tangent<T> ub)
+// {
+//     using std::clamp;
+//
+//     return { clamp(v.v, lb.v, ub.v), lb.d * (v.v < lb.v) + ub.d * (v.v > ub.v) };
+// }
+//
+// template<typename T>
+// fn tangent<T> mid(tangent<T> v, tangent<T> lb, tangent<T> ub)
+// {
+//     return clamp(v, lb, ub);
+// }
+//
+// template<typename T>
+// fn tangent<T> sin(tangent<T> x)
+// {
+//     using std::cos;
+//     using std::sin;
+//
+//     return { sin(x.v), cos(x.v) * x.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> cos(tangent<T> x)
+// {
+//     using std::cos;
+//     using std::sin;
+//
+//     return { cos(x.v), -sin(x.v) * x.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> tan(tangent<T> x)
+// {
+//     using std::pow;
+//     using std::tan;
+//
+//     return { tan(x.v), static_cast<T>(1.0) + pow(tan(x.v), 2) };
+// }
+//
+// template<typename T>
+// fn tangent<T> asin(tangent<T> x)
+// {
+//     using std::asin;
+//     using std::pow;
+//     using std::sqrt;
+//
+//     return { asin(x.v), x.d / sqrt(1.0 - pow(x.v, 2)) };
+// }
+//
+// template<typename T>
+// fn tangent<T> acos(tangent<T> x)
+// {
+//     using std::acos;
+//     using std::pow;
+//     using std::sqrt;
+//
+//     return { acos(x.v), -x.d / sqrt(1.0 - pow(x.v, 2)) };
+// }
+//
+// template<typename T>
+// fn tangent<T> atan(tangent<T> x)
+// {
+//     using std::atan;
+//     using std::pow;
+//
+//     return { atan(x.v), x.d / (1.0 + pow(x.v, 2)) };
+// }
+//
+// template<typename T>
+// fn tangent<T> atan2(tangent<T> a, tangent<T> b)
+// {
+//     using std::atan2;
+//     using std::pow;
+//
+//     return { atan2(a.v, b.v), (a.d * b.v - b.d * a.v) / (pow(a.v, 2) + pow(b.v, 2)) };
+// }
+//
+// template<typename T>
+// fn tangent<T> atan2(tangent<T> a, T b)
+// {
+//     using std::atan2;
+//     using std::pow;
+//
+//     return { atan2(a.v, b), b * a.d / (pow(a.v, 2) + pow(b, 2)) };
+// }
+//
+// template<typename T>
+// fn tangent<T> atan2(T a, tangent<T> b)
+// {
+//     using std::atan2;
+//     using std::pow;
+//
+//     return { atan2(a, b.v), -a * b.d / (pow(a, 2) + pow(b.v, 2)) };
+// }
+//
+// template<typename T>
+// fn tangent<T> sinh(tangent<T> x)
+// {
+//     using std::cosh;
+//     using std::sinh;
+//
+//     return { sinh(x.v), cosh(x.v) * x.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> cosh(tangent<T> x)
+// {
+//     using std::cosh;
+//     using std::sinh;
+//
+//     return { cosh(x.v), sinh(x.v) * x.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> tanh(tangent<T> x)
+// {
+//     using std::cosh;
+//     using std::pow;
+//     using std::tanh;
+//
+//     return { tanh(x.v), x.d / (pow(cosh(x.v), 2)) };
+// }
+//
+// template<typename T>
+// fn tangent<T> asinh(tangent<T> x)
+// {
+//     using std::asinh;
+//     using std::pow;
+//     using std::sqrt;
+//
+//     return { asinh(x.v), x.d / sqrt(pow(x.v, 2) + 1.0) };
+// }
+//
+// template<typename T>
+// fn tangent<T> acosh(tangent<T> x)
+// {
+//     using std::acosh;
+//     using std::pow;
+//     using std::sqrt;
+//
+//     return { acosh(x.v), x.d / sqrt(pow(x.v, 2) - 1.0) };
+// }
+//
+// template<typename T>
+// fn tangent<T> atanh(tangent<T> x)
+// {
+//     using std::atanh;
+//     using std::pow;
+//
+//     return { atanh(x.v), x.d / (1.0 - pow(x.v, 2)) };
+// }
+//
+// template<typename T>
+// fn tangent<T> exp(tangent<T> x)
+// {
+//     using std::exp;
+//
+//     return { exp(x.v), exp(x.v) * x.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> log(tangent<T> x)
+// {
+//     using std::log;
+//
+//     // NOTE: We currently do not treat the case where x.v == 0, x.d != 0 to map to -+inf.
+//     return { log(x.v), x.d / x.v };
+// }
+//
+// template<typename T>
+// fn tangent<T> log2(tangent<T> x)
+// {
+//     using std::log2;
+//
+//     return { log2(x.v), x.d / (x.v * std::numbers::ln2_v<T>)};
+// }
+//
+// template<typename T>
+// fn tangent<T> log10(tangent<T> x)
+// {
+//     using std::log10;
+//
+//     return { log10(x.v), x.d / (x.v * std::numbers::ln10_v<T>)};
+// }
+//
+// template<typename T>
+// fn tangent<T> sqr(tangent<T> x)
+// {
+//     return { sqr(x.v), 2.0 * x.v * x.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> sqrt(tangent<T> x)
+// {
+//     using std::numeric_limits;
+//     using std::sqrt;
+//     // NOTE: We currently do not treat the case where x.v == 0, x.d > 0 to map to +inf.
+//     return { sqrt(x.v), x.d / (2.0 * sqrt(x.v) + (x.v == static_cast<T>(0.0) ? numeric_limits<T>::min() : 0.0)) };
+// }
+//
+// template<typename T>
+// fn tangent<T> pown(tangent<T> x, auto n)
+// {
+//     using std::pow;
+//
+//     return { pow(x.v, n), n * pow(x.v, n - 1) * x.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> pown(auto x, tangent<T> n)
+// {
+//     using std::pow;
+//
+//     return { pow(x, n.v), pow(x, n.v) * log(x) * n.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> pow(tangent<T> x, auto n)
+// {
+//     return pown(x, n);
+// }
+//
+// template<typename T>
+// fn tangent<T> pow(auto x, tangent<T> n)
+// {
+//     return pown(x, n);
+// }
+//
+// template<typename T>
+// fn tangent<T> pow(tangent<T> x, tangent<T> n)
+// {
+//     using std::log;
+//     using std::pow;
+//
+//     return { pow(x.v, n.v),
+//              n.v * pow(x.v, n.v - 1) * x.d + pow(x.v, n.v) * log(x.v) * n.d };
+// }
+//
+// template<typename T>
+// fn tangent<T> cbrt(tangent<T> x)
+// {
+//     using std::cbrt;
+//
+//     return { cbrt(x.v), x.d / (static_cast<T>(3.0) * sqr(cbrt(x.v))) };
+// }
+//
+// template<typename T>
+// fn tangent<T> ceil(tangent<T> x)
+// {
+//     using std::ceil;
+//
+//     return { ceil(x.v), 0.0 };
+// }
+//
+// template<typename T>
+// fn T rint(tangent<T> x)
+// {
+//     using std::rint;
+//
+//     return rint(x.v);
+// }
+//
+// template<typename T>
+// fn long lrint(tangent<T> x)
+// {
+//     using std::lrint;
+//
+//     return lrint(x.v);
+// }
+//
+// template<typename T>
+// fn long long llrint(tangent<T> x)
+// {
+//     using std::llrint;
+//
+//     return llrint(x.v);
+// }
+//
+// template<typename T>
+// fn tangent<T> floor(tangent<T> x)
+// {
+//     using std::floor;
+//
+//     return { floor(x.v), 0.0 };
+// }
+//
+// template<typename T>
+// fn bool isinf(tangent<T> x)
+// {
+//     using ::isinf;
+//     using std::isinf;
+//
+//     return isinf(x.v);
+// }
+//
+// template<typename T>
+// fn bool isfinite(tangent<T> x)
+// {
+//     using std::isfinite;
+//
+//     return isfinite(x.v);
+// }
+//
+// template<typename T>
+// fn bool isnan(tangent<T> a)
+// {
+//     using std::isnan;
+//
+//     return isnan(a.v);
+// }
+//
+// template<typename T>
+// fn tangent<T> remquo(tangent<T> x, tangent<T> y, int *quo)
+// {
+//     using std::remquo;
+//
+//     return { remquo(x.v, y.v, quo), 0.0 };
+// }
+//
+// template<typename T>
+// fn bool signbit(tangent<T> x)
+// {
+//     using std::signbit;
+//
+//     return signbit(x.v);
+// }
+//
+// template<typename T>
+// fn tangent<T> copysign(tangent<T> mag, T sgn)
+// {
+//     using std::copysign;
+//
+//     return { copysign(mag.v, sgn), copysign(mag.d, sgn) };
+// }
+
+#undef fn
+
+} // namespace cu
+
+#endif // CUTANGENT_ARITHMETIC_BASIC_V_CUH
