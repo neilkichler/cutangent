7a89ec3dbb23ab9a7f633921c9074f330a765c90
Neil Kichler <neil.kichler@gmail.com>
2024-09-20 18:31:40 +0200

    add option for vector load and fix allocated shared memory per block

--- examples/mccormick/mccormick_v.cu
+++ examples/mccormick/mccormick_v.cu
@@ -17,7 +17,10 @@ using cu::tangents;
 
 #define PRINT_DEBUG 0
 
-constexpr __device__ auto f(auto x, auto y, auto z, auto w)
+#define USE_VECTOR_LOAD_128 0
+#define USE_VECTOR_LOAD_256 0
+
+constexpr __device__ auto f(const auto &x, const auto &y, const auto &z, const auto &w)
 {
     auto print = [](auto &x) {
 #if PRINT_DEBUG
@@ -42,14 +45,19 @@ constexpr __device__ auto f(auto x, auto y, auto z, auto w)
 
     print(x);
     print(y);
-    auto a = x + y;
-    print(a);
+    auto a = x + y + z + w;
+    auto b = a + a + a + a;
+    auto c = b + b + b;
+    auto d = c + c;
+    // print(a);
 
-    return a;
+    return d;
 }
 
-template<typename T, int N, int M>
-__global__ void kernel(T *in, T *out, int n_elems, int n_vars)
+template<typename T, int N>
+__global__ void
+// __launch_bounds__(256, 1)
+kernel(T *in, T *out, int n_elems, int n_vars)
 {
     extern __shared__ cu::mccormick<cu::tangents<T, N>> xs[];
 
@@ -68,6 +76,7 @@ __global__ void kernel(T *in, T *out, int n_elems, int n_vars)
     // maybe they store in the same location in the second loop?
 
     // block range when considering only mccormick values (for initial copy from global memory)
+    // int n_elems_per_block = (n_elems + n_blocks - 1) >> int(log2(n_blocks));
     int n_elems_per_block = (n_elems + n_blocks - 1) / n_blocks;
     int block_start       = n_elems_per_block * bid * 4 * n_vars;
     int block_end         = min(block_start + n_elems_per_block * 4 * n_vars, n * 4);
@@ -78,37 +87,57 @@ __global__ void kernel(T *in, T *out, int n_elems, int n_vars)
     int t_block_end                     = min(t_block_start + n_elems_per_block_with_tangents, n_elems * n_vars * n_doubles_per_mc);
 
     // if (tid == 0)
-    //     printf("[gid:%3d][bid:%3d][tid:%3d][vid:%3d][xid:%3d] elems_per_block: %3d block_start: %3d block_end: %3d\n",
-    //            gid, bid, tid, vid, xid, n_elems_per_block, block_start, block_end);
+    //     printf("[gid:%3d][bid:%3d][tid:%3d][xid:%3d] elems_per_block: %3d block_start: %3d block_end: %3d\n",
+    //            gid, bid, tid, xid, n_elems_per_block, block_start, block_end);
 
     // seed tangents
+    // TODO: unroll
     for (int i = tid + t_block_start; i < t_block_end; i += n_threads) {
         int v = i / n_doubles_per_mc;
+#if PRINT_DEBUG
+        printf("[gid:%3d][bid:%3d][tid:%3d][vid:%3d][xid:%3d][i:%3d] n_elems_tangent: %3d t_block_start: %3d t_block_end: %3d\n",
+               gid, bid, tid, v, xid, i, n_elems_per_block_with_tangents, t_block_start, t_block_end);
+#endif
 
-        int tangent_idx = i % (N + 1) - 1;                                      // tangent index for this thread, -1 is no tangent but a value to be skipped
+        int tangent_idx = (i % (N + 1)) - 1;                                    // tangent index for this thread, -1 is no tangent but a value to be skipped
                                                                                 // faster alternative: int tangent_idx = x - floor(1/(N+1) * x) * (N+1) - 1; // TODO: check if compiler figures this out
         bool is_cv_or_cc                  = i % n_doubles_per_mc < 2 * (N + 1); // 2 since we only seed cv and cc
         ((double *)xs)[i - t_block_start] = (v % n_vars == tangent_idx) && is_cv_or_cc ? 1.0 : 0.0;
 
-        printf("[gid:%3d][bid:%3d][tid:%3d][vid:%3d][xid:%3d][i:%3d] n_elems_tangent: %3d t_block_start: %3d t_block_end: %3d\n",
-               gid, bid, tid, v, xid, i, n_elems_per_block_with_tangents, t_block_start, t_block_end);
-
 #if PRINT_DEBUG
-        printf("[iter:%3d][gid:%3d][bid:%3d][tid:%3d][vid:%3d][tangent_idx:%3d][xid:%3d][i:%3d] tangent seed value: %g\n",
-               iter, gid, bid, tid, v, tangent_idx, xid, i - t_block_start, ((double *)xs)[i - t_block_start]);
+        printf("[gid:%3d][bid:%3d][tid:%3d][vid:%3d][tangent_idx:%3d][xid:%3d][i:%3d] tangent seed value: %g\n",
+               gid, bid, tid, v, tangent_idx, xid, i - t_block_start, ((double *)xs)[i - t_block_start]);
 #endif
     }
 
     __syncthreads();
 
     // Load elements from global memory into shared memory trying to get a balanced allocation in all blocks
-
-    // TODO: could create a range out of block start and end to remove manual for loop
+#if USE_VECTOR_LOAD_128
+    for (int i = tid * 2 + block_start; i + 1 < block_end; i += n_threads) {
+        int sid                       = (i - block_start) * (N + 1);
+        double2 tmp                   = *(double2 *)&in[i]; // init value
+        ((double *)xs)[sid]           = tmp.x;
+        ((double *)xs)[sid + (N + 1)] = tmp.y;
+        // printf("[gid:%3d][bid:%3d][tid:%3d][i:%3d] init value: %g\n", gid, bid, tid, i, in[i]);
+    }
+#elif USE_VECTOR_LOAD_256
+    for (int i = tid * 4 + block_start; i + 3 < block_end; i += n_threads) {
+        int sid                           = (i - block_start) * (N + 1);
+        double4 tmp                       = *(double4 *)&in[i]; // init value
+        ((double *)xs)[sid]               = tmp.x;
+        ((double *)xs)[sid + (N + 1)]     = tmp.y;
+        ((double *)xs)[sid + 2 * (N + 1)] = tmp.z;
+        ((double *)xs)[sid + 3 * (N + 1)] = tmp.w;
+        // printf("[gid:%3d][bid:%3d][tid:%3d][i:%3d] init value: %g\n", gid, bid, tid, i, in[i]);
+    }
+#else
     for (int i = tid + block_start; i < block_end; i += n_threads) {
         int sid             = (i - block_start) * (N + 1);
         ((double *)xs)[sid] = in[i]; // init value
-        // TODO: maybe use vector load (double2 or double4) for increased throughput? But we already use doubles
+        // printf("[gid:%3d][bid:%3d][tid:%3d][i:%3d] init value: %g\n", gid, bid, tid, i, in[i]);
     }
+#endif
 
     __syncthreads();
 
@@ -116,6 +145,7 @@ __global__ void kernel(T *in, T *out, int n_elems, int n_vars)
     int compute_out_offset  = n_elems_per_block * n_vars;
     int compute_block_start = n_elems_per_block * bid * N;
     int compute_block_end   = min(compute_block_start + n_elems_per_block * N, n_elems * N);
+    // TODO: unroll
     for (int i = tid + compute_block_start; i < compute_block_end; i += n_threads) {
         int lid = xid * n_vars - bid * n_threads;    // local variable id
         int sid = (i - compute_block_start) % N;     // shared memory tangent id (subtracting compute_blocK_start is not really needed since we are starting at a multiple of N)
@@ -144,6 +174,7 @@ __global__ void kernel(T *in, T *out, int n_elems, int n_vars)
     int out_sh_mem_offset = compute_out_offset * n_doubles_per_mc;
     int out_block_start   = n_elems_per_block * bid * n_doubles_per_mc;
     int out_block_end     = min(out_block_start + n_elems_per_block * n_doubles_per_mc, n_elems * n_doubles_per_mc);
+    // TODO: unroll
     for (int i = tid + out_block_start; i < out_block_end; i += n_threads) {
         int sid = out_sh_mem_offset + i - out_block_start;
         out[i]  = ((double *)xs)[sid];
@@ -170,18 +201,20 @@ we should first try to make use of all the blocks individually doing one mccormi
 
 int main()
 {
-    constexpr int n_elems = 13;
-    constexpr int n_vars  = 4;
+    constexpr int n_elems = 256;
+    constexpr int n_vars  = 32;
     // constexpr int n_copy_doubles_per_mc = 4 + 2 * n_vars; // the number of doubles to copy from device back to host per mccormick relaxation. Skips box derivatives. Take cv, cc, lb, ub, cv.ds, cc.ds
     constexpr int n_copy_doubles_per_mc = 4 + 4 * n_vars; // the number of doubles to copy from device back to host per mccormick relaxation. Skips box derivatives. Take cv, cc, lb, ub, cv.ds, cc.ds
 
-    constexpr int n_blocks  = 4;
+    constexpr int n_blocks  = 256;
     constexpr int n_threads = 256;
 
-    constexpr int n_tangents  = 4;                                      // the number of tangents to perform per mccormick relaxation
-    constexpr int n_mccormick = n_vars * std::ceil(n_elems / n_blocks); // the number of mccormick relaxations to access in shared memory per block
+    constexpr int n_tangents = 32; // the number of tangents to perform per mccormick relaxation
 
-    static_assert(n_mccormick >= n_vars, "n_mccormick must be >= n_vars");
+    constexpr int n_elems_per_block = std::ceil(double(n_elems) / n_blocks);
+    constexpr int n_vars_per_block  = n_vars * n_elems_per_block; // the number of mccormick variables to access in shared memory per block
+
+    assert(n_mccormick >= n_vars && "n_mccormick must be >= n_vars");
     static_assert(n_tangents <= n_vars, "n_tangents must be <= n_vars");
 
     cu::mccormick<double> xs[n_elems * n_vars] {};
@@ -202,8 +235,8 @@ int main()
 #endif
 
     // int n_bytes_shared = min(n_mccormick, n_elems * n_vars) * 4 * sizeof(double) * (min(n_tangents, n_vars) + 1); // TODO: what if n_vars > n_tangents?
-    constexpr int n_bytes_shared_in  = n_mccormick * 4 * sizeof(double) * (n_tangents + 1); // TODO: what if n_vars > n_tangents?
-    constexpr int n_bytes_shared_out = n_elems * 4 * sizeof(double) * (n_tangents + 1);
+    constexpr int n_bytes_shared_in  = n_vars_per_block * 4 * sizeof(double) * (n_tangents + 1); // TODO: what if n_vars > n_tangents?
+    constexpr int n_bytes_shared_out = n_elems_per_block * 4 * sizeof(double) * (n_tangents + 1);
     constexpr int n_bytes_shared     = n_bytes_shared_in + n_bytes_shared_out;
     printf("n_bytes_shared = %d B\n", n_bytes_shared);
 
@@ -218,34 +251,17 @@ int main()
     double *h_xs;
     CUDA_CHECK(cudaMallocHost(&h_xs, n_elems * n_vars * sizeof(*xs))); // 4 because of cv, cc, lb, ub
     memcpy(h_xs, xs, n_elems * n_vars * sizeof(*xs));
-    // for (int i = 0; i < n_elems * n_vars * 4; i++) {
-    // xs_soa[i] = ((double *)xs)[i];
-    // std::cout << xs_soa[i] << std::endl;
-    // }
-
-    // double *xs_soa;
-    // CUDA_CHECK(cudaMallocHost(&xs_soa, n_elems * n_vars * sizeof(*xs))); // 4 because of cv, cc, lb, ub
-    // for (int i = 0; i < n_elems * n_vars; i++) {
-    //     xs_soa[i + 0 * (n_elems * n_vars)] = xs[i].cv;
-    //     xs_soa[i + 1 * (n_elems * n_vars)] = xs[i].cc;
-    //     xs_soa[i + 2 * (n_elems * n_vars)] = xs[i].box.lb;
-    //     xs_soa[i + 3 * (n_elems * n_vars)] = xs[i].box.ub;
-    // }
-
-    // #if PRINT_DEBUG
-    //     for (auto x : xs_soa) {
-    //         std::cout << "x[soa] is: " << x << std::endl;
-    //     }
-    // #endif
 
     CUDA_CHECK(cudaMemcpy(d_xs, h_xs, n_elems * n_vars * sizeof(*xs), cudaMemcpyHostToDevice));
-    kernel<double, n_tangents, n_mccormick><<<n_blocks, n_threads, n_bytes_shared>>>(d_xs, d_res, n_elems, n_vars);
+    kernel<double, n_tangents><<<n_blocks, n_threads, n_bytes_shared>>>(d_xs, d_res, n_elems, n_vars);
     CUDA_CHECK(cudaMemcpy(res, d_res, (n_elems * n_copy_doubles_per_mc) * sizeof(*d_res), cudaMemcpyDeviceToHost));
     CUDA_CHECK(cudaDeviceSynchronize());
 
+#if 0 || PRINT_DEBUG
     for (auto r : res) {
         std::cout << r << std::endl;
     }
+#endif
 
     CUDA_CHECK(cudaFree(d_xs));
     CUDA_CHECK(cudaFree(d_res));
