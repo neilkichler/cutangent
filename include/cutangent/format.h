e6062cda6f7ad97a12a36e1cfd4a037a6fe16094
Neil Kichler <neil.kichler@gmail.com>
2025-07-23 18:36:41 +0200

    add support for std::format

--- include/cutangent/format.h
+++ include/cutangent/format.h
@@ -3,6 +3,7 @@
 
 #include <cutangent/tangent.h>
 
+#include <format>
 #include <ostream>
 
 namespace cu
@@ -15,5 +16,20 @@ std::ostream &operator<<(std::ostream &os, tangent<T> x)
 }
 
 } // namespace cu
+
+template<typename T>
+struct std::formatter<cu::tangent<T>> : std::formatter<T>
+{
+    auto format(const cu::tangent<T> &x, std::format_context &ctx) const
+    {
+        auto out = ctx.out();
+
+        out = std::format_to(out, "{{v: ");
+        out = std::formatter<T>::format(x.v, ctx);
+        out = std::format_to(out, ", d: ");
+        out = std::formatter<T>::format(x.d, ctx);
+        return std::format_to(out, "}}");
+    }
+};
 
 #endif // CUTANGENT_FORMAT_H
