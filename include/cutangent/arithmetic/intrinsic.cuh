598c38fc4e68555a8bca211212679024f57d198d
Neil Kichler <neil.kichler@gmail.com>
2025-07-24 18:24:09 +0200

    cleanup unused intrinsic declarations

--- include/cutangent/arithmetic/intrinsic.cuh
+++ include/cutangent/arithmetic/intrinsic.cuh
@@ -27,7 +27,6 @@ namespace cu::intrinsic
     template<typename T> inline __device__ T median    (T x, T y);
     template<typename T> inline __device__ T min       (T x, T y);
     template<typename T> inline __device__ T max       (T x, T y);
-    template<typename T> inline __device__ T copy_sign (T x, T y);
     template<typename T, typename U> inline __device__ T next_after(T x, U y);
     template<typename T> inline __device__ T rcp_down  (T x);
     template<typename T> inline __device__ T rcp_up    (T x);
@@ -38,10 +37,6 @@ namespace cu::intrinsic
     template<typename T> inline __device__ T trunc     (T x);
     template<typename T> inline __device__ T round_away(T x);
     template<typename T> inline __device__ T round_even(T x);
-    template<typename T> inline __device__ T exp       (T x);
-    template<typename T> inline __device__ T exp10     (T x);
-    template<typename T> inline __device__ T exp2      (T x);
-    template<typename T> inline __device__ __host__ T nan();
     template<typename T> inline __device__ T pos_inf();
     template<typename T> inline __device__ T neg_inf();
     template<typename T> inline __device__ T next_floating(T x);
@@ -62,7 +57,6 @@ namespace cu::intrinsic
     template<> inline __device__ tangent<double> median    (tangent<double> x, tangent<double> y)                    { return (x + y) * .5; }
     template<> inline __device__ tangent<double> min       (tangent<double> x, tangent<double> y)                    { return min(x, y); }
     template<> inline __device__ tangent<double> max       (tangent<double> x, tangent<double> y)                    { return max(x, y); }
-    // template<> inline __device__ double copy_sign (double x, double y) { return copysign(x, y); }
     template<> inline __device__ tangent<double> next_after(tangent<double> x, tangent<double> y) { using std::nextafter; return { nextafter(x.v, y.v), x.d }; }
     template<> inline __device__ tangent<double> rcp_down  (tangent<double> x) { using std::pow; return { __drcp_rd(x.v), - __dmul_rd(pow(x.v, -2.0), x.d) }; }
     template<> inline __device__ tangent<double> rcp_up    (tangent<double> x) { using std::pow; return { __drcp_ru(x.v), - __dmul_ru(pow(x.v, -2.0), x.d) }; }
@@ -70,13 +64,8 @@ namespace cu::intrinsic
     template<> inline __device__ tangent<double> sqrt_up   (tangent<double> x) { auto sqrt_x_v = sqrt_up  (x.v); return { sqrt_x_v, div_up  (x.d, add_down(2.0 * sqrt_x_v, x.v == 0.0 ? std::numeric_limits<double>::min() : 0.0)) }; }
     template<> inline __device__ tangent<double> int_down  (tangent<double> x) { return floor(x); }
     template<> inline __device__ tangent<double> int_up    (tangent<double> x) { return ceil(x); }
-    // template<> inline __device__ double trunc     (double x)           { return ::trunc(x); }
     // template<> inline __device__ double round_away(double x)           { return round(x); }
     // template<> inline __device__ double round_even(double x)           { return nearbyint(x); }
-    template<> inline __device__ tangent<double> exp      (tangent<double> x) { return { ::exp(x.v), ::exp(x.d) }; }
-    // template<> inline __device__ double exp10     (double x)           { return ::exp10(x); }
-    // template<> inline __device__ double exp2      (double x)           { return ::exp2(x); }
-    template<> inline __device__ __host__ tangent<double> nan() { return { ::nan(""), ::nan("") }; }
     template<> inline __device__ tangent<double> neg_inf() { return { __longlong_as_double(0xfff0000000000000ull), __longlong_as_double(0xfff0000000000000ull) }; }
     template<> inline __device__ tangent<double> pos_inf() { return { __longlong_as_double(0x7ff0000000000000ull), __longlong_as_double(0x7ff0000000000000ull) }; }
     template<> inline __device__ tangent<double> next_floating(tangent<double> x) { return { nextafter(x.v, pos_inf<tangent<double>>().v), nextafter(x.d, pos_inf<tangent<double>>().d) }; }
