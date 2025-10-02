f73b0f5f9043d7e58ecb6f669d10a60a2a91c163
Neil Kichler <neil.kichler@gmail.com>
2025-10-02 13:40:14 +0200

    minor: add const ref overloads for value & derivative

--- include/cutangent/tangent.h
+++ include/cutangent/tangent.h
@@ -52,6 +52,12 @@ constexpr T &value(tangent<T> &x)
 }
 
 template<typename T>
+constexpr const T &value(const tangent<T> &x)
+{
+    return x.v;
+}
+
+template<typename T>
 constexpr T value(tangent<T> &&x)
 {
     return std::move(x.v);
@@ -59,6 +65,12 @@ constexpr T value(tangent<T> &&x)
 
 template<typename T>
 constexpr T &derivative(tangent<T> &x)
+{
+    return x.d;
+}
+
+template<typename T>
+constexpr const T &derivative(const tangent<T> &x)
 {
     return x.d;
 }
